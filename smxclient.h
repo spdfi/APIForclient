e58d7f3a90acf63eb0473ed9e8b922f82c7a9589
ShishirAudentus <shishir@audentus.com>
2017-03-17 18:13:13 +0530

    Comments for dynamic and static linking
    Please edit the file appropriately for static or dynamic linking.

--- smxclient.h
+++ smxclient.h
@@ -18,6 +18,7 @@ extern "C"
 // BEGIN APIs always return without delays and can be used safely without
 // any bearing on added latency. 
 
+//For Dynamic linking eg dll. NOT for shared lib.
 // SDT = Service Setup Time
 SMXAPI void SMXCALL SMX_Begin_SST(void);
 SMXAPI void SMXCALL SMX_End_SST(void);
@@ -34,6 +35,24 @@ SMXAPI void SMXCALL SMX_End_CUT(void);
 SMXAPI void SMXCALL SMX_Begin_CUS(void);
 SMXAPI void SMXCALL SMX_End_CUS(char *);
 
+//If using shared lib. NOT for dynamic linking eg dll
+/* 
+// SDT = Service Setup Time
+SMXAPI void SMXCALL SMX_Begin_SST(void);
+SMXAPI void SMXCALL SMX_End_SST(void);
+
+// SPT = Service Processing Time
+SMXAPI void SMXCALL SMX_Begin_SPT(void);
+SMXAPI void SMXCALL SMX_End_SPT(void);
+
+// CUT = Context Update Time.
+SMXAPI void SMXCALL SMX_Begin_CUT(void);
+SMXAPI void SMXCALL SMX_End_CUT(void);
+
+//$ CST = Custom Update .
+SMXAPI void SMXCALL SMX_Begin_CUS(void);
+SMXAPI void SMXCALL SMX_End_CUS(char *);
+*/
 
 #ifdef __cplusplus
 } 
